d7ade5a20b17ad1f5830ffe82178912c64d377c7
EfeEriomala <eeriomala@byupathway.edu>
2025-03-19 19:44:04 +0000

    Commiting exercise 2 for the first time

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your  grade percentage? ");
+        string answer =Console.ReadLine();
+        int score = int.Parse(answer);
+
+        string letter = "";
+        
+        if (score >= 90)
+        {
+            letter = "A";
+        }
+        else if (score >= 80)
+        {
+            letter = "B";
+        }
+        else if (score >= 70)
+        {
+            letter = "C";
+        }
+        else if (score >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is :{letter}");
+
+        if (score >= 70)
+        {
+            Console.WriteLine($"Congratulations! You got a: {letter}  you Passed.");
+        }
+        else
+        {
+            Console.WriteLine($"Sorry. You got a: {letter} You didnt make the cut. Try Harder next time");
+        }
     }
 }
