3d3658763ae16480dcb370fdd5cb135182c2f092
EfeEriomala <eeriomala@byupathway.edu>
2025-09-05 04:49:01 +0100

    Added  a comment

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -3,10 +3,12 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
+    {   
+        // Ask the user for their first name and last name
         Console.WriteLine("What is your firt name?");
         string firstname = Console.ReadLine();
 
+        // Ask the user for their last name
         Console.WriteLine("What is your last name?");
         string lastname = Console.ReadLine();
 
