c293e10a1614506e29a8f4e2c8ae6aebc4a63894
EfeEriomala <eeriomala@byupathway.edu>
2025-09-05 04:43:39 +0100

    my first real comit

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your firt name?");
+        string firstname = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {firstname}, {firstname} {lastname}.");
     }
 }
