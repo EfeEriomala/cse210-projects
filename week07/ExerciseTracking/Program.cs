0176a799babed9ed23b3ec179fa85929af1076aa
Efe Eriomala <eeriomala@gmail.com>
2025-12-10 14:25:57 +0100

    completed assignment

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(new DateTime(2025, 12, 8), 30, 3.0),
+            new Cycling(new DateTime(2025, 12, 5), 45, 15.0),
+            new Swimming(new DateTime(2025, 12, 3), 40, 30)
+        };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
